206e467993af00d786504f2a8875cf296cf98b5d
Natasha Ramos <natasha.ramos5@upr.edu>
2023-11-13 14:15:24 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+const app = express();
+const fs = require("fs");
+const userController = require("./controllers/userController");
+
+
+app.use(express.json());
+app.use('/users', userController);
+
+function writeTo(file, data) {
+  fs.writeFile(file, JSON.stringify(data), "utf8", (err) => {
+    throw err;
+  });
+}
+
+function readFrom(file) {
+  if (fs.existsSync(file)) {
+    let data = fs.readFileSync(file, "utf-8");
+    data = JSON.parse(data);
+    return data;
+  }
+}
+
+
+app.listen(3000, () => {
+  console.log("Project is running!");
+})
+
+
+
+
