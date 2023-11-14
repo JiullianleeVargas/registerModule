c67cacd66b4e6ab0209744c80c892fe3f3fb8dd5
javierlemuel <98832519+javierlemuel@users.noreply.github.com>
2023-11-14 13:07:14 -0400

    changes to DB version

--- controllers/userController2.js
+++ controllers/userController2.js
@@ -1,56 +1,82 @@
+//Initialize server requirements, including MongoDB
 const express = require("express");
-const fs = require("fs");
 const user = express();
 const { MongoClient } = require('mongodb');
 
+//Initialize requirements for bcrypt password hashing
+const bcrypt = require('bcrypt');
+const saltRounds = 10; // Number of salt rounds for bcrypt hashing
+
+//Export user so our app route can import
 module.exports = user;
 
-function writeTo(file, data) {
-  fs.writeFile(file, JSON.stringify(data), "utf8", (err) => {
-    throw err;
-  });
+//Function for verifying if a string is a valid email
+function isEmail(email) {
+  const emailChecker = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  
+  return emailChecker.test(email);
 }
 
-function readFrom(file) {
-  if (fs.existsSync(file)) {
-    let data = fs.readFileSync(file, "utf-8");
-    data = JSON.parse(data);
-    return data;
+//Function for password hashing using bcrypt
+async function hashPassword(password) {
+  try {
+      // Generate a salt
+      const salt = await bcrypt.genSalt(saltRounds);
+
+      // Hash the password with the generated salt
+      const hashedPassword = await bcrypt.hash(password, salt);
+
+      return hashedPassword;
+  } catch (error) {
+      throw error;
   }
 }
 
 
 user.use(express.json());
 
-
-
+//Register async function
 user.post('/register', async (req, res) => {
 
-  const client = new MongoClient('mongodb://localhost:27017');
+  //Connect to MongoDB client
+  const client = new MongoClient('mongodb://0.0.0.0:27017');
 
   try {
 
+      //Receive an email and password strings
       let email = req.body.email; 
       let password = req.body.password;
 
       email = email.trim();
       password = password.trim();
 
-      if(email != "" && password != "")
+      //Hash the password
+      password = hashPassword(password);
+
+      //Check that input fields both contain data
+      if(email != "" && password != "") 
       {
+        //Send an error message if email isn't in valid format
+        if(!isEmail(email)) {
+            res.send("Invalid email input!");
+        }
+
+          //Connect to DB named 'task_management' and collection named 'users'
           client.connect();
           const database = client.db('task_management');
           const usersCollection = database.collection('users');
 
+          //Find query for that specific user given the email
           const user = await usersCollection.findOne({ email });
 
+          //If user exists, send an error message
           if (user) {
-
-              //if user found, cookie message = username/email already exists
               res.send('Email already exists!');
-              //res.cookie('message', 'Email already exists!');
-          } else {
-            const user = {
+          } else 
+          {
+            //If user doesn't exist, create that user in the DB's users collection
+            const user = 
+            {
                 email: email,
                 password: password
             };
@@ -59,20 +85,18 @@ user.post('/register', async (req, res) => {
               res.send('Account created!');
           }
       }
-      else
-      {
+      //Send an error if either input field is empty
+      else {
           res.send('Email and password cannot be empty!');
-          //res.cookie('message', 'Email and password cannot be empty!');
       }
-
-      //res.redirect('/landingPage.html');
-      
-
-  } catch (err) {
-      res.send(err);
-      //console.error(err);
-   } finally {
-      await client.close();
+  } 
+  //Catch any other errors
+  catch (err) {
+    res.send(err);
+  } 
+  //Close MongoDB client at end
+  finally {
+    await client.close();
   }
   
 })
